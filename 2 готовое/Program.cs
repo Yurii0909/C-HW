571a5f73b7471af246968b2b943d182fa9d4c2f0
Yurii <Yurii>
2022-11-26 22:22:34 +0400

    1

---
+++ 2 готовое/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Задание 1");
+int a = 5;
+int b = 7;
+if(b > a)
+  Console.WriteLine("B больше");
+else
+  Console.WriteLine("A больше");
+a = default(a);
+a = default(b);
+int a = 5;
+int b = 7;
+if(b > a)
+  Console.WriteLine("B больше");
+else
+  Console.WriteLine("A больше");
+
+  Console.WriteLine("Задание 3");
