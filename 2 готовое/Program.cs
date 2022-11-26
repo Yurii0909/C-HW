053dd4d07aea36d968fc8cfcbd9fa7e53ca48380
Yurii <Yurii>
2022-11-26 23:37:05 +0400

    2

--- 2 готовое/Program.cs
+++ 2 готовое/Program.cs
@@ -1,17 +1,54 @@
-﻿Console.WriteLine("Задание 1");
+﻿        Console.WriteLine("Задача 2");
 int a = 5;
 int b = 7;
-if(b > a)
-  Console.WriteLine("B больше");
+if (b > a)
+    Console.WriteLine($"max = {b}");
 else
-  Console.WriteLine("A больше");
-a = default(a);
-a = default(b);
-int a = 5;
-int b = 7;
-if(b > a)
-  Console.WriteLine("B больше");
+    Console.WriteLine($"max = {a}");
+a = 2;
+b = 10;
+if (b > a)
+    Console.WriteLine($"max = {b}");
 else
-  Console.WriteLine("A больше");
+    Console.WriteLine($"max = {a}");
+a = -9;
+b = -3;
+if (b > a)
+    Console.WriteLine($"max = {b}");
+else
+    Console.WriteLine($"max = {a}");
 
-  Console.WriteLine("Задание 3");
+
+    Console.WriteLine("Задача 4");
+int z = 2;
+int x = 3;
+int c = 7;
+int max = z;
+if (x>max) max=x;
+if (c>max) max=c;
+Console.WriteLine(max);
+
+z = 44;
+x = 5;
+c = 78;
+max = z;
+if (x>max) max=x;
+if (c>max) max=c;
+Console.WriteLine(max);
+
+z = 22;
+x = 3;
+c = 9;
+max = z;
+if (x>max) max=x;
+if (c>max) max=c;
+Console.WriteLine(max);
+
+ Console.WriteLine("Задание 6");
+double c=1
+ double d=2 
+
+Console.WriteLine("Задание 8");
+int f = new Random().Next(1, 8)
+(f%2 ==0)
+Console.WriteLine("A больше")
